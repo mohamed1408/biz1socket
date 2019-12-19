fa4766e7d6c4c58126d8c09eed148e3cc381f5cd
mohamed1408 <mohamedanastsi@gmail.com>
2019-12-19 16:04:29 +0530

    fghgh

--- index.js
+++ index.js
@@ -9,22 +9,42 @@ const INDEX = path.join(__dirname, 'index.html');
 
 // Start server
 const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
- .listen(PORT, () => console.log("Listening on localhost:" + PORT));
+  .use((req, res) => res.sendFile(INDEX))
+  .listen(PORT, () => console.log("Listening on localhost:" + PORT));
 
 // Initiatlize SocketIO
 const io = socketIO(server);
 
 // Register "connection" events to the WebSocket
-io.on("connection", function(socket) {
+io.on("connection", function (socket) {
+  let previousId;
+  const safeJoin = currentId => {
+    socket.leave(previousId);
+    socket.join(currentId);
+    previousId = currentId;
+  };
   // Register "join" events, requested by a connected client
-  socket.on("join", function (room) {
-    // join channel provided by client
-    socket.join(room)
-    // Register "image" events, sent by the client
-    socket.on("image", function(msg) {
-      // Broadcast the "image" event to all other clients in the room
-      socket.broadcast.to(room).emit("image", msg);
-    });
+  socket.on("getDoc", function (room) {
+    safeJoin(docId);
+    socket.emit("document", documents[docId]);
   })
+  socket.on("addDoc", function (room) {
+    documents[doc.order.details.id] = doc;
+    safeJoin(doc.id);
+    socket.emit("document", doc);
+  })
+  socket.on("editDoc", function (room) {
+    documents[doc.order.details.id] = doc;
+    socket.to(doc.id).emit("document", doc);
+  })
+  socket.on("changeSts", function (room) {
+    orders[doc.Id] = doc;
+    io.emit("Orders", Object.values(orders));
+  })
+  socket.on("Orders", function (room) {
+    orders[doc.Id] = doc;
+    io.emit("Orders", Object.values(orders));
+  })
+  io.emit("Orders", Object.values(orders));
+  io.emit("documents", Object.values(documents));
 });
