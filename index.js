03fe1412c6194f1347cabf97e41a7aa0ec42e2b5
mohamed1408 <mohamedanastsi@gmail.com>
2019-12-19 16:48:04 +0530

    ghghghghghghghghghghgh

--- index.js
+++ index.js
@@ -17,14 +17,26 @@ const io = socketIO(server);
 
 // Register "connection" events to the WebSocket
 io.on("connection", function(socket) {
+  const safeJoin = currentId => {
+    socket.leave(previousId);
+    socket.join(currentId);
+    previousId = currentId;
+  };
+
   // Register "join" events, requested by a connected client
   socket.on("join", function (room) {
     // join channel provided by client
-    socket.join(room)
+    socket.join(room, () => console.log("room"))
     // Register "image" events, sent by the client
     socket.on("image", function(msg) {
       // Broadcast the "image" event to all other clients in the room
       socket.broadcast.to(room).emit("image", msg);
     });
   })
+
+
+
+  // socket.on("getDoc", function(doc) {
+
+  // })
 });
