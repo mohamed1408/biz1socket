3fb56bc617b89453d44dea5ca4c3ec5308e7ad3a
mohamed1408 <mohamedanastsi@gmail.com>
2019-12-19 15:54:34 +0530

    sdsdf

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app1.use(bodyParser.json({strict: false}));
 
 app1.get('/', (request, response) =>  response.send(`hello!`));
 
-app1.listen(5000, () => console.info('Application running on port 3000'));
+app1.listen(5000, () => console.info('Application running on port 5000'));
 
 app1.post('/api/data', (request, response) => {
   var postBody = request.body;
