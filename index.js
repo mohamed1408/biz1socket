0e3746ec4a3bffbda1f5b04bbba9c4a7ee6a31a8
mohamed1408 <mohamedanastsi@gmail.com>
2019-12-19 15:57:16 +0530

    cccc

--- index.js
+++ index.js
@@ -55,30 +55,3 @@ io.on("connection", socket => {
 
 
 ///////////
-const bodyParser = require('body-parser');
-const app1 = express();
-app1.use(bodyParser.json({strict: false}));
-
-app1.get('/', (request, response) =>  response.send(`hello!`));
-
-app1.listen(5000, () => console.info('Application running on port 5000'));
-
-app1.post('/api/data', (request, response) => {
-  var postBody = request.body;
-  response.send("data received");
-  postBody.order.details.accept = 0
-  var json = postBody;
-  documents[postBody.order.details.id] = json;
-  console.log(documents);
-  io.emit("documents", Object.values(documents));
-  });
-
-  app1.post('/api/order', (request, response) => {
-    var postBody = request.body;
-    response.send("data received");
-    postBody.order.details.accept = 0
-    var json = postBody;
-    documents[postBody.order.details.id] = json;
-    console.log(documents);
-    io.emit("Orders", Object.values(documents));
-    });
