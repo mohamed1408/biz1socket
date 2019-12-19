f70d69119bdae8a60933ae1f778cbed14a120c6f
mohamed1408 <mohamedanastsi@gmail.com>
2019-12-19 15:46:05 +0530

    dgfdfg

--- index.js
+++ index.js
@@ -16,15 +16,69 @@ const server = express()
 const io = socketIO(server);
 
 // Register "connection" events to the WebSocket
-io.on("connection", function(socket) {
-  // Register "join" events, requested by a connected client
-  socket.on("join", function (room) {
-    // join channel provided by client
-    socket.join(room)
-    // Register "image" events, sent by the client
-    socket.on("image", function(msg) {
-      // Broadcast the "image" event to all other clients in the room
-      socket.broadcast.to(room).emit("image", msg);
-    });
-  })
+io.on("connection", socket => {
+  let previousId;
+  const safeJoin = currentId => {
+    socket.leave(previousId);
+    socket.join(currentId);
+    previousId = currentId;
+  };
+
+  socket.on("getDoc", docId => {
+    safeJoin(docId);
+    socket.emit("document", documents[docId]);
+  });
+
+  socket.on("addDoc", doc => {
+    documents[doc.order.details.id] = doc;
+    safeJoin(doc.id);
+    socket.emit("document", doc);
+  });
+
+  socket.on("editDoc", doc => {
+    documents[doc.order.details.id] = doc;
+    socket.to(doc.id).emit("document", doc);
+  });
+
+  socket.on("changeSts", doc => {
+    orders[doc.Id] = doc;
+    io.emit("Orders",Object.values(orders));
+  });
+
+  socket.on("Orders", doc => {
+    orders[doc.Id] = doc;
+    io.emit("Orders",Object.values(orders));
+  });
+  io.emit("Orders",Object.values(orders));
+  io.emit("documents", Object.values(documents));
 });
+
+
+///////////
+const bodyParser = require('body-parser');
+const app1 = express();
+app1.use(bodyParser.json({strict: false}));
+
+app1.get('/', (request, response) =>  response.send(`hello!`));
+
+app1.listen(3000, () => console.info('Application running on port 3000'));
+
+app1.post('/api/data', (request, response) => {
+  var postBody = request.body;
+  response.send("data received");
+  postBody.order.details.accept = 0
+  var json = postBody;
+  documents[postBody.order.details.id] = json;
+  console.log(documents);
+  io.emit("documents", Object.values(documents));
+  });
+
+  app1.post('/api/order', (request, response) => {
+    var postBody = request.body;
+    response.send("data received");
+    postBody.order.details.accept = 0
+    var json = postBody;
+    documents[postBody.order.details.id] = json;
+    console.log(documents);
+    io.emit("Orders", Object.values(documents));
+    });
