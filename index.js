124a451b055102f02f567a8514a43286dbcc67aa
mohamed1408 <mohamedanastsi@gmail.com>
2019-12-19 16:09:21 +0530

    sdfsdf

--- index.js
+++ index.js
@@ -1,50 +1,76 @@
-// Import packages
-const express = require("express");
-const socketIO = require("socket.io");
-const path = require("path");
+const express = require('express');
+const bodyParser = require('body-parser');
+const app1 = express();
+app1.use(bodyParser.json({strict: false}));
 
-// Configuration
-const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
+app1.get('/', (request, response) =>  response.send(`hello!`));
 
-// Start server
-const server = express()
-  .use((req, res) => res.sendFile(INDEX))
-  .listen(PORT, () => console.log("Listening on localhost:" + PORT));
+app1.listen(3000, () => console.info('Application running on port:3000'));
 
-// Initiatlize SocketIO
-const io = socketIO(server);
-
-// Register "connection" events to the WebSocket
-io.on("connection", function (socket) {
-  let previousId;
-  const safeJoin = currentId => {
-    socket.leave(previousId);
-    socket.join(currentId);
-    previousId = currentId;
-  };
-  // Register "join" events, requested by a connected client
-  socket.on("getDoc", function (room) {
-    safeJoin(docId);
-    socket.emit("document", documents[docId]);
-  })
-  socket.on("addDoc", function (room) {
-    documents[doc.order.details.id] = doc;
-    safeJoin(doc.id);
-    socket.emit("document", doc);
-  })
-  socket.on("editDoc", function (room) {
-    documents[doc.order.details.id] = doc;
-    socket.to(doc.id).emit("document", doc);
-  })
-  socket.on("changeSts", function (room) {
-    orders[doc.Id] = doc;
-    io.emit("Orders", Object.values(orders));
-  })
-  socket.on("Orders", function (room) {
-    orders[doc.Id] = doc;
-    io.emit("Orders", Object.values(orders));
-  })
-  io.emit("Orders", Object.values(orders));
+app1.post('/api/data', (request, response) => {
+  var postBody = request.body;
+  response.send("data received");
+  postBody.order.details.accept = 0
+  var json = postBody;
+  documents[postBody.order.details.id] = json;
+  console.log(documents);
   io.emit("documents", Object.values(documents));
+  });
+
+  app1.post('/api/order', (request, response) => {
+    var postBody = request.body;
+    response.send("data received");
+    postBody.order.details.accept = 0
+    var json = postBody;
+    documents[postBody.order.details.id] = json;
+    console.log(documents);
+    io.emit("Orders", Object.values(documents));
+    });
+/////
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+const documents = [];
+const orders = [];
+
+io.on("connection", socket => {
+    let previousId;
+    const safeJoin = currentId => {
+      socket.leave(previousId);
+      socket.join(currentId);
+      previousId = currentId;
+    };
+  
+    socket.on("getDoc", docId => {
+      safeJoin(docId);
+      socket.emit("document", documents[docId]);
+    });
+  
+    socket.on("addDoc", doc => {
+      documents[doc.order.details.id] = doc;
+      safeJoin(doc.id);
+      socket.emit("document", doc);
+    });
+  
+    socket.on("editDoc", doc => {
+      documents[doc.order.details.id] = doc;
+      socket.to(doc.id).emit("document", doc);
+    });
+
+    socket.on("changeSts", doc => {
+      orders[doc.Id] = doc;
+      io.emit("Orders",Object.values(orders));
+    });
+
+    socket.on("Orders", doc => {
+      orders[doc.Id] = doc;
+      io.emit("Orders",Object.values(orders));
+    });
+    io.emit("Orders",Object.values(orders));
+    io.emit("documents", Object.values(documents));
+  });
+
+  http.listen(4444, () => {
+    console.log("4444");
 });
