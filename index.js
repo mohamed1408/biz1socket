0da19d9875de99ce3a9d08a74bd9d6fd9f4afdeb
mohamed1408 <mohamedanastsi@gmail.com>
2019-12-23 14:53:02 +0530

    dfgdfg

--- index.js
+++ index.js
@@ -1,43 +1,76 @@
-// Import packages
-const express = require("express");
-const socketIO = require("socket.io");
-const path = require("path");
+// Setup basic express server
+var express = require('express');
+var app = express();
+var path = require('path');
+var server = require('http').createServer(app);
+var io = require('../..')(server);
+var port = process.env.PORT || 3000;
 
-// Configuration
-const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
+server.listen(port, () => {
+  console.log('Server listening at port %d', port);
+});
 
-// Start server
-const server = express()
-  .use((req, res) => res.sendFile(INDEX))
-  .listen(PORT, () => console.log("Listening on localhost:" + PORT));
+// Routing
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Initiatlize SocketIO
-const io = socketIO(server);
+// Chatroom
 
-// Register "connection" events to the WebSocket
-io.on("connection", function (socket) {
-  // const safeJoin = currentId => {
-  //   socket.leave(previousId);
-  //   socket.join(currentId);
-  //   previousId = currentId;
-  // };
+var numUsers = 0;
 
-  // Register "join" events, requested by a connected client
-  socket.on("join", function (room) {
-    // join channel provided by client
-    socket.join(room, () => console.log("room"))
-    // Register "image" events, sent by the client
-    socket.on("image", function (msg) {
-      // Broadcast the "image" event to all other clients in the room
-      socket.broadcast.to(room).emit("image", msg);
+io.on('connection', (socket) => {
+  var addedUser = false;
+
+  // when the client emits 'new message', this listens and executes
+  socket.on('new message', (data) => {
+    // we tell the client to execute 'new message'
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
     });
-  })
+  });
 
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', (username) => {
+    if (addedUser) return;
 
+    // we store the username in the socket session for this client
+    socket.username = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
 
-  // socket.on("getDoc", function (docId) {
-  //   safeJoin(docId);
-  //   socket.emit("document", documents[docId]);
-  // })
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', () => {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', () => {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', () => {
+    if (addedUser) {
+      --numUsers;
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
+  });
 });
